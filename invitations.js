239b9041a9a72233c59ae4c07255e7fb9aed5757
Ronald Acarapi <64119163+ron981010@users.noreply.github.com>
2025-06-07 18:48:30 -0500

    go prices

--- invitations.js
+++ invitations.js
@@ -8,11 +8,33 @@ function initInvitationsPage() {
     console.log(">> initInvitationsPage ejecutada");
 
     invitaciones = [
-        { titulo: "Newspaper", imagen: "assets/phone1.png", precio: "S/. 120", demo: "#", categoria: "bodas" },
-        { titulo: "Envelope", imagen: "assets/phone2.png", precio: "S/. 100", demo: "#", categoria: "bodas" },
-        { titulo: "Pearls", imagen: "assets/pearls.png", precio: "S/. 150", demo: "#", categoria: "otros" },
-        { titulo: "Book", imagen: "assets/card.png", precio: "S/. 130", demo: "#", categoria: "cumpleanos" },
-        { titulo: "XV", imagen: "assets/xv.png", precio: "S/. 180", demo: "#", categoria: "xv" }
+        // Bodas
+        { titulo: "Estilo Periódico", imagen: "assets/phone1.png", precio: "S/. 80", demo: "#", categoria: "bodas" },
+        { titulo: "Estilo Sobre", imagen: "assets/phone2.png", precio: "S/. 80", demo: "#", categoria: "bodas" },
+        { titulo: "Pearlas Rosadas", imagen: "assets/pearls.png", precio: "S/. 100", demo: "#", categoria: "bodas" },
+        { titulo: "Estilo Libro", imagen: "assets/card.png", precio: "S/. 80", demo: "#", categoria: "bodas" },
+        { titulo: "Sobre verde", imagen: "assets/phone1.png", precio: "S/. 110", demo: "https://giftclick-david-y-grecia.netlify.app/index.html?Grecia%20y-Acompa%C3%B1ante-2-", categoria: "bodas" },
+        { titulo: "Flores Celestes", imagen: "assets/phone1.png", precio: "S/. 110", demo: "https://giftclick.github.io/jherifer-selene/sobre", categoria: "bodas" },
+        { titulo: "Flores beige", imagen: "assets/phone1.png", precio: "S/. 100", demo: "#", categoria: "bodas" },
+
+        // XV
+        { titulo: "XV - Princesa y el sapo", imagen: "assets/xv.png", precio: "S/. 80", demo: "#", categoria: "xv" },
+        { titulo: "XV - Rapunzel", imagen: "assets/xv.png", precio: "S/. 80", demo: "https://mikaela-xv.netlify.app/", categoria: "xv" },
+        { titulo: "XV - La Bella y la Bestia", imagen: "assets/xv.png", precio: "S/. 80", demo: "https://giftclick.github.io/ariana", categoria: "xv" },
+        { titulo: "XV - Flores Celestes", imagen: "assets/xv.png", precio: "S/. 80", demo: "https://giftclick.github.io/esperanza/index.html", categoria: "xv" },
+        { titulo: "XV - Estrellas", imagen: "assets/xv.png", precio: "S/. 90", demo: "https://giftclick.github.io/dalia", categoria: "xv" },
+        { titulo: "XV - Amarillo Flores", imagen: "assets/xv.png", precio: "S/. 80", demo: "https://giftclick.github.io/estrella", categoria: "xv" },
+        { titulo: "XV - La bella durmiente", imagen: "assets/xv.png", precio: "S/. 80", demo: "#", categoria: "xv" },
+        // Cumpleaños
+        { titulo: "Cumpleaños - Niña Rosa", imagen: "assets/card.png", precio: "S/. 60", demo: "https://giftclick.github.io/carmen", categoria: "cumpleanos" },
+        { titulo: "Paw Control y Plin Plin", imagen: "assets/card.png", precio: "S/. 60", demo: "https://giftclick-enzo-issac.netlify.app/", categoria: "cumpleanos" },
+        { titulo: "Plin Plin Zafari", imagen: "assets/card.png", precio: "S/. 60", demo: "#", categoria: "cumpleanos" },
+        // Otros
+        { titulo: "Baby Shower - Varón", imagen: "assets/phone2.png", precio: "S/. 60", demo: "https://giftclick.github.io/ezio/", categoria: "otros" },
+        { titulo: "Corte de pelo - Fiesta Niña", imagen: "assets/card.png", precio: "S/. 60", demo: "https://giftclick.github.io/heymi/", categoria: "otros" },
+        { titulo: "Bautizo - Varón", imagen: "assets/phone2.png", precio: "S/. 60", demo: "https://giftclick.github.io/ezio/", categoria: "otros" },
+        { titulo: "Misa de honra", imagen: "assets/phone2.png", precio: "S/. 60", demo: "#", categoria: "otros" },
+        { titulo: "Pedida de mano - Rosas Rojas", imagen: "assets/pearls.png", precio: "S/. 60", demo: "https://giftclick.github.io/flavio-alicia/", categoria: "otros" }
     ];
 
     renderInvitaciones("todos");
